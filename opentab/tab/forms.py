dc2dd6b771eb82dd39613ac9e9e9db3a51d4699e
omar jandali <omar jandali>
2017-06-13 17:00:13 -0700

    Commit: fixed some errors and added files
    Explain:
    The first change was done in the settings file. I had to add some changes
    to the middleware portion becasue tehre was an error that was occuring
    with the modules. The sencond major change was the change in the forms
    file. I changed the Create group form as well as added the form for adding
    users at each level of the users count. this was so that based on what the
    user selects, displays a specific form.

--- opentab/tab/forms.py
+++ opentab/tab/forms.py
@@ -9,6 +9,46 @@ class CreateGroupForm(forms.Form):
     image = fomrs.ImageField()
     member_count = forms.IntegerField()
 
+class AddMembersFourForm(forms.Form):
+    user_one = forms.CharField(max_length = 25)
+    user_two = forms.CharField(max_length = 25)
+    user_three = forms.CharField(max_length = 25)
+    user_four = forms.CharField(max_length = 25)
+
+class AddMembersSevenForm(forms.Form):
+    user_one = forms.CharField(max_length = 25)
+    user_two = forms.CharField(max_length = 25)
+    user_three = forms.CharField(max_length = 25)
+    user_four = forms.CharField(max_length = 25)
+    user_five = forms.CharField(max_length = 25)
+    user_sex = forms.CharField(max_length = 25)
+    user_seven = forms.CharField(max_length = 25)
+
+class AddMembersTenForm(forms.Form):
+    user_one = forms.CharField(max_length = 25)
+    user_two = forms.CharField(max_length = 25)
+    user_three = forms.CharField(max_length = 25)
+    user_four = forms.CharField(max_length = 25)
+    user_five = forms.CharField(max_length = 25)
+    user_sex = forms.CharField(max_length = 25)
+    user_seven = forms.CharField(max_length = 25)
+    user_eight = forms.CharField(max_length = 25)
+    user_nine = forms.CharField(max_length = 25)
+    user_ten = forms.CharField(max_length = 25)
+
+class AddMembersElevenForm(forms.Form):
+    user_one = forms.CharField(max_length = 25)
+    user_two = forms.CharField(max_length = 25)
+    user_three = forms.CharField(max_length = 25)
+    user_four = forms.CharField(max_length = 25)
+    user_five = forms.CharField(max_length = 25)
+    user_sex = forms.CharField(max_length = 25)
+    user_seven = forms.CharField(max_length = 25)
+    user_eight = forms.CharField(max_length = 25)
+    user_nine = forms.CharField(max_length = 25)
+    user_ten = forms.CharField(max_length = 25)
+    user_eleven = forms.CharField(max_length = 25)
+
 class CreateRecordForm(forms.Form):
     amount = forms.FloatField()
     description = fomrs.CharField(max_length = 200)
